980d7173d62859e5c8f732fcfcef13b42570874c
Katofln <frederikhstroem@gmail.com>
2018-01-02 18:01:42 +0100

    Added tilemap and fixed up a few things

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// Game size.
+width = 640;
+height = 480;
+
+// Tilewidth (px)
+var tileWidth = 32
+
 function getNewFallVelocity (gravity, yVelocity) {
   return gravity + yVelocity;
 }
+
+function objectCollidingWithTilemap (x, y) {
+
+}
+
+// Get canvas context.
+var canvas = document.getElementById("gameCanvas");
+var canvasContext = canvas.getContext("2d");
