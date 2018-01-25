ad61a159e9568f8ee9563722ad5ea2431c179e3d
BuildTools <frederikhstroem@gmail.com>
2018-01-25 10:32:28 +0100

    Progress on rendering

--- js/Game.render.js
+++ js/Game.render.js
@@ -1,4 +1,25 @@
 Game.render = function () {
+  // Clear canvas.
+  canvasContext.clearRect(0, 0, width, height);
+
   // Draw tilemap.
-  
+  var currentRow = 1;
+  var currentCol = 1;
+  var currentTileX = 0;
+  var currentTileY = 0;
+  for (var i = 0; i < Tilemap.map.length; i++) {
+    if (Tilemap.map[i] == 1) {
+      canvasContext.drawImage(groundTile, currentTileX, currentTileY);
+    }
+
+    if (currentCol != Tilemap.columns) {
+      currentCol++;
+      currentTileX += Tilemap.tileSize;
+    } else {
+      currentCol = 1;
+      currentTileX = 0;
+      currentTileY += Tilemap.tileSize;
+      currentRow++;
+    }
+  }
 }
