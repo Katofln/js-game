09af03e285de3d9a92d253cd2a6dfe173faf405d
Katofln <mail@frederikstroem.com>
2018-01-27 23:01:25 +0100

    Key event listener done

---
+++ js/keyEventListener.js
@@ -0,0 +1,84 @@
+// Holds current value of what is pressed down.
+var IsKeyDown = {
+    leftKey:false,
+    rightKey:false,
+    upKey:false,
+    dotKey:false,
+    commaKey:false,
+    aKey:false,
+    dKey:false,
+    wKey:false,
+    cKey:false,
+    vKey:false,
+};
+
+// Called everytime key is pressed.
+document.addEventListener('keydown', function(event) {
+  // Switch statement to set keys pressed down.
+  switch (event.keyCode) {
+    case 37:
+      IsKeyDown.leftKey = true;
+      break;
+    case 39:
+      IsKeyDown.rightKey = true;
+      break;
+    case 38:
+      IsKeyDown.upKey = true;
+      break;
+    case 190:
+      IsKeyDown.dotKey = true;
+      break;
+    case 188:
+      IsKeyDown.commaKey = true;
+      break;
+    case 65:
+      IsKeyDown.aKey = true;
+      break;
+    case 68:
+      IsKeyDown.dKey = true;
+      break;
+    case 87:
+      IsKeyDown.wKey = true;
+      break;
+    case 67:
+      IsKeyDown.cKey = true;
+      break;
+    case 86:
+      IsKeyDown.vKey = true;
+  }
+});
+
+// Called everytime key is released.
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 37:
+      IsKeyDown.leftKey = false;
+      break;
+    case 39:
+      IsKeyDown.rightKey = false;
+      break;
+    case 38:
+      IsKeyDown.upKey = false;
+      break;
+    case 190:
+      IsKeyDown.dotKey = false;
+      break;
+    case 188:
+      IsKeyDown.commaKey = false;
+      break;
+    case 65:
+      IsKeyDown.aKey = false;
+      break;
+    case 68:
+      IsKeyDown.dKey = false;
+      break;
+    case 87:
+      IsKeyDown.wKey = false;
+      break;
+    case 67:
+      IsKeyDown.cKey = false;
+      break;
+    case 86:
+      IsKeyDown.vKey = false;
+  }
+});
