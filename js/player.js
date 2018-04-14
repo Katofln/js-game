a3ae243cf50ac0b84ccfec5adc918c6bf7e49f33
Katofln <mail@frederikstroem.com>
2018-04-14 14:01:37 +0200

    Typo

--- js/player.js
+++ js/player.js
@@ -287,7 +287,7 @@ function Player (x, y, animationDirection) {
         this.tickTimeToNewShuriken = Game.currentRenderTick + this.tickTimeBetweenNewShurikens;
       }
       /*
-        End of shurkens.
+        End of shurikens.
       */
 
       /*
