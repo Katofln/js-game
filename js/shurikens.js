fb30d5e05d687212df1a032d38db98fec04fed8c
Katofln <mail@frederikstroem.com>
2018-04-15 21:46:37 +0200

    Fixed basic shuriken collision

--- js/shurikens.js
+++ js/shurikens.js
@@ -1,6 +1,6 @@
 function updateShurikens (shurikens, targetPlayer) {
   for (var i = 0; i < shurikens.length; i++) {
-    if (isObjectsColliding(shurikens[1], shurikens[2], Game.shurikenWidth, Game.shurikenHeight, targetPlayer.x, targetPlayer.y, targetPlayer.width, targetPlayer.height)) {
+    if (isObjectsColliding(shurikens[i][1], shurikens[i][2], Game.shurikenWidth, Game.shurikenHeight, targetPlayer.x, targetPlayer.y, targetPlayer.width, targetPlayer.height)) {
       console.log("Shuriken collision");
     }
   }
