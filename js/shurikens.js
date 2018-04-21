8efc556bfe021c8bfd23d9f9e75f9872b15950dc
Katofln <mail@frederikstroem.com>
2018-04-21 16:57:40 +0200

    Update to shurikens

--- js/shurikens.js
+++ js/shurikens.js
@@ -1,12 +1,21 @@
-function updateShurikens (shurikens, targetPlayer) {
-  for (var i = 0; i < shurikens.length; i++) {
-    if (isObjectsColliding(shurikens[i][1], shurikens[i][2], Game.shurikenWidth, Game.shurikenHeight, targetPlayer.x, targetPlayer.y, targetPlayer.width, targetPlayer.height)) {
+function updateShurikens (player, targetPlayer) {
+  for (var i = 0; i < player.shurikensInAir.length; i++) {
+    // Move and check if colliding with tilemap.
+    // Right
+    /*if () {
+
+    // Left
+    } else if () {
+
+    }*/
+    // Check if colliding with player.
+    if (isObjectsColliding(player.shurikensInAir[i][1], player.shurikensInAir[i][2], Game.shurikenWidth, Game.shurikenHeight, targetPlayer.x, targetPlayer.y, targetPlayer.width, targetPlayer.height)) {
       console.log("Shuriken collision");
     }
   }
 }
 
 function updateAllShurikens () {
-  updateShurikens(player1.shurikensInAir, player2);
-  updateShurikens(player2.shurikensInAir, player1);
+  updateShurikens(player1, player2);
+  updateShurikens(player2, player1);
 }
