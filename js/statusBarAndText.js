16750f08bbf71f2812ad389b1ba31d58cf4d095f
Katofln <mail@frederikstroem.com>
2018-04-24 18:36:01 +0200

    status bar and text function layout added

---
+++ js/statusBarAndText.js
@@ -0,0 +1,11 @@
+function drawPlayerStatus (player, drawSide) {  // Draw side takes "left" or "right", means which side/player status is drawn for.
+
+}
+
+function drawPlayerXWon (player) { // Takes input: "1" and "2".
+
+}
+
+function drawNewGameStarting () {
+
+}
