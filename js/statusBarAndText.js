332f9c95d8668b8fcf6103a44c916ae57e372875
Katofln <mail@frederikstroem.com>
2018-04-24 20:09:30 +0200

    Larger text

--- js/statusBarAndText.js
+++ js/statusBarAndText.js
@@ -3,14 +3,14 @@ function drawPlayerStatus (player, drawSide) {  // Draw side takes "left" or "ri
 }
 
 function drawPlayerXWon (player) { // Takes input: "1" and "2".
-  canvasContext.font = "30px " + Game.standardTextFont;
+  canvasContext.font = "37px " + Game.standardTextFont;
   canvasContext.textAlign = "center";
   canvasContext.fillStyle = Game.standardTextColor;
   canvasContext.fillText("Player " + player + " won round " + Game.rounds + "!", Game.width/2, Game.height/2);
 }
 
 function drawPlayersNeedToReadyUp () {
-  canvasContext.font = "30px " + Game.standardTextFont;
+  canvasContext.font = "34px " + Game.standardTextFont;
   canvasContext.textAlign = "center";
   canvasContext.fillStyle = Game.standardTextColor;
   canvasContext.fillText("Hold attack/block key to accept ready check.", Game.width/2, Game.height/2);
