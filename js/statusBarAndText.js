b1ad074c85b88dc1e35ea6bdc9e8c8f12179fd23
Katofln <mail@frederikstroem.com>
2018-04-25 13:24:24 +0200

    Ready check more fancy

--- js/statusBarAndText.js
+++ js/statusBarAndText.js
@@ -23,10 +23,17 @@ function drawPlayersNeedToReadyUp () {
   canvasContext.font = "34px " + Game.standardTextFont;
   canvasContext.textAlign = "center";
   canvasContext.fillStyle = Game.standardTextColor;
+  // Hightligt text if one player is ready.
   if (player1.shurikenKey) {
     canvasContext.fillStyle = "#cd0000";
   } else if (player2.shurikenKey) {
     canvasContext.fillStyle = "#0000cd";
   }
-  canvasContext.fillText('Hold shuriken key to accept ready check (v + .)', Game.width/2, Game.height/2);
+
+  canvasContext.fillText('Hold shuriken key to accept ready check', Game.width/2, Game.height/2);
+
+  // Draw which keys to hold down.
+  canvasContext.font = "20px " + Game.standardTextFont;
+  canvasContext.fillStyle = Game.standardTextColor;
+  canvasContext.fillText('Keys: v + .', Game.width/2, Game.height/2 + 30);
 }
