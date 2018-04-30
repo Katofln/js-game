d2a2cb1e6cd53b59d3c1fbfe5e5204ee93b0f1bc
Katofln <mail@frederikstroem.com>
2018-05-01 00:28:26 +0200

    Placeholders for values added

--- js/settingsPanel.js
+++ js/settingsPanel.js
@@ -1,7 +1,28 @@
 var settingsPanelCurrentlyDisplayed = false;
 
 // Insert standard values.
-// TODO
+// Player 1
+document.getElementById("player1HP").placeholder = player1.hp;
+document.getElementById("player1Gravity").placeholder = player1.gravity;
+document.getElementById("player1MoveVelocity").placeholder = player1.moveVelocity;
+document.getElementById("player1JumpVelocity").placeholder = player1.jumpVelocity;
+document.getElementById("player1MaxJumpTimes").placeholder = player1.maxJumpTimes;
+document.getElementById("player1ShurikenDamage").placeholder = player1.shurikenDamage;
+document.getElementById("player1ShurikenVelocity").placeholder = player1.shurikenVelocity;
+document.getElementById("player1MaxShurikens").placeholder = player1.maxShurikens;
+document.getElementById("player1TimeBetweenShurikenThrows").placeholder = player1.tickTimeBetweenShurikenThrows;
+document.getElementById("player1TimeBetweenNewShurikens").placeholder = player1.tickTimeBetweenNewShurikens;
+// Player 2
+document.getElementById("player2HP").placeholder = player2.hp;
+document.getElementById("player2Gravity").placeholder = player2.gravity;
+document.getElementById("player2MoveVelocity").placeholder = player2.moveVelocity;
+document.getElementById("player2JumpVelocity").placeholder = player2.jumpVelocity;
+document.getElementById("player2MaxJumpTimes").placeholder = player2.maxJumpTimes;
+document.getElementById("player2ShurikenDamage").placeholder = player2.shurikenDamage;
+document.getElementById("player2ShurikenVelocity").placeholder = player2.shurikenVelocity;
+document.getElementById("player2MaxShurikens").placeholder = player2.maxShurikens;
+document.getElementById("player2TimeBetweenShurikenThrows").placeholder = player2.tickTimeBetweenShurikenThrows;
+document.getElementById("player2TimeBetweenNewShurikens").placeholder = player2.tickTimeBetweenNewShurikens;
 
 function showSettingsPanel (bool) {  // true/false
   if (bool) {
