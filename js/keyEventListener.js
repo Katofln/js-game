35eff617f7196e1a51082356e43edbec1663fd35
Katofln <mail@frederikstroem.com>
2018-04-30 23:29:15 +0200

    Fixed settings key

--- js/keyEventListener.js
+++ js/keyEventListener.js
@@ -1,8 +1,5 @@
 // Holds current value of what is pressed down.
 var IsKeyDown = {
-  // General.
-  zeroKey:false,
-
   // Player 1.
   aKey:false,
   dKey:false,
@@ -51,8 +48,6 @@ document.addEventListener('keydown', function(event) {
       break;
     case 49:
       IsKeyDown.oneKey = true;
-    case 48:
-      IsKeyDown.zeroKey = true;
   }
 });
 
@@ -88,8 +83,6 @@ document.addEventListener('keyup', function(event) {
       break;
     case 49:
       IsKeyDown.oneKey = false;
-    case 48:
-      IsKeyDown.zeroKey = false;
   }
 });
 
--- js/settingsPanel.js
+++ js/settingsPanel.js
@@ -1,1 +1,7 @@
 var settingsPanelCurrentlyDisplayed = false;
+
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 48) {
+    console.log("Settings pane key.");
+  }
+});
