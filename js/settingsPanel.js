952104456468a86fb625d22de18d333aeff319cf
Katofln <mail@frederikstroem.com>
2018-04-30 23:55:04 +0200

    Settings panel can now be toggled

--- js/settingsPanel.js
+++ js/settingsPanel.js
@@ -1,9 +1,27 @@
 var settingsPanelCurrentlyDisplayed = false;
 
+function showSettingsPanel (bool) {  // true/false
+  if (bool) {
+    document.getElementById("settingsPanel").style.display = "block";
+    settingsPanelCurrentlyDisplayed = true;
+  } else {
+    document.getElementById("settingsPanel").style.display = "none";
+    settingsPanelCurrentlyDisplayed = false;
+  }
+}
+
 document.addEventListener('keydown', function(event) {
   if (event.keyCode == 48) {
-    // TODO
-
-    settingsPanelCurrentlyDisplayed = true;
+    if (settingsPanelCurrentlyDisplayed) {
+      showSettingsPanel(false);
+    } else {
+      showSettingsPanel(true);
+    }
   }
 });
+
+function saveSettingsPanel () {
+  // TODO
+
+  showSettingsPanel(false);
+}
